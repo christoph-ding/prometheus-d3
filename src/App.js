3b68178c4157206d1fc25f898cfba7486441cb06
Pokegear <christoph.ding@gmail.com>
2016-12-18 18:10:39 -0500

    Have a country list component that makes a list item for each item in data in app component.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const sampleData = ['chris', 'steve', 'joe', 'bob'];
+
 export class WorldDataApp extends Component {
-  render() {
+  constructor(props){
+    // Pass props to parent class
+    super(props);
+    // Set initial state
+    this.state = {
+      countries: sampleData
+    }
+  }
+
+  // get data from api
+  // componentDidMount(){
+
+  // }
+
+  render(){
     return (
       <div className="WorldDataApp">
         <h1> Entire World Data App </h1>
-        <CountryList />
+        <h1> hey this is some data: {this.state.countries[0]}</h1>
+        <CountryList countries={this.state.countries}/>
       </div>
     );
   }
 }
 
 export class CountryList extends Component {
-  render() {
+  render(){
     return (
       <div className="CountryList">
         <h1> List of Countries </h1>
+          <ul>
+            {
+              this.props.countries.map(function(country) {
+                return (
+                  <h1> {country} </h1>
+                )
+              })
+            }          
+          </ul>
       </div>
     );
   }
